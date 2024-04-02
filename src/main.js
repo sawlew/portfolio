6b9004cab055527c757b098ea1a46e0110508955
Soliu Adeosun <sawlew@gmail.com>
2024-04-02 11:16:28 +0100

    mailer integration

--- src/main.js
+++ src/main.js
@@ -15,14 +15,14 @@ $("#navbar a").on("click", function (event) {
   });
 
 
-function resetHeight(){
-  // reset the body height to that of the inner browser
-  document.body.style.height = window.innerHeight + "px";
-}
+// function resetHeight(){
+//   // reset the body height to that of the inner browser
+//   document.body.style.height = window.innerHeight + "px";
+// }
 // reset the height whenever the window's resized
-window.addEventListener("resize", resetHeight);
+// window.addEventListener("resize", resetHeight);
 // called to initially set the height.
-resetHeight();
+// resetHeight();
 
 
 // Drop-down menu for mobile design
@@ -87,3 +87,4 @@ function noGitHub(){
   }, 3000);
   
 }
+
