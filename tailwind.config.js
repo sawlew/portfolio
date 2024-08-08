8cca077e332b0aa71be29b3d79069729f3434973
Soliu Adeosun <sawlew@gmail.com>
2024-08-08 17:33:24 +0100

    professions modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,30 @@ module.exports = {
       
       'xs': {'max': '314px'},
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'text-slide-2': 'text-slide-2 5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+      },
+      keyframes: {
+        'text-slide-2': {
+          '0%, 26.66%': {
+            transform: 'translateY(0%)',
+        },
+        '33.33%, 60%': {
+            transform: 'translateY(-33.33%)',
+        },
+        '66.66%, 93.33%': {
+            transform: 'translateY(-66.66%)',
+        },
+        '100%': {
+            transform: 'translateY(-99.99%)',
+        },
+        },
+      },
+    },
   },
   plugins: [],
 }
